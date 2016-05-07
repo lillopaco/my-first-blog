df8318c022b452cce78416acee03e553bd972ca3
fiorella <deluca_fiorella@yahoo.it>
2016-05-07 18:07:39 +0200

    modification indentationerror

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 def post_new(request):
-     if request.method == "POST":
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
